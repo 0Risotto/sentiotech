9bb07f96db3cff9499f5e6d0eb652565d41d1c9e
basil ahmad <bas0227843@ju.edu.jo>
2025-10-01 02:26:15 +0000

    * updated training_iterator function * added some number to confirm pipeline reflects the insights about the file. * updated the trian / test function to reflect results better and to provide better insights * remove the warning on the torchaudio.load * added visualization for the post-splitting distribution * overall review and bug fixes * the code ran completely and properly

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from extract_classes import iter_wavs, detect_and_parse
 from dataloader import AudioDataset
 from utils import Compose, Extract3channels, training_iterator, train, test
 from models import TestModel
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
+import warnings
+warnings.filterwarnings("ignore")
 # HYPERPARAMETERS == START ==
 SAMPLING_RATE = 48000
 CLIP_LENGTH = 3
@@ -19,10 +22,11 @@ HOP_LENGTH = 128
 N_MELS = 128
 # preprocessing hyperparameters
 
-SPLITS = [0.8, 0.1, 0.1]
+SPLITS = [0.01, 0.01, 0.98]
 BATCH_SIZE = 8
 EPOCHS = 1
 TESTING_FREQUENCY = 1
+DEVICE = "cpu"
 # traininig hyperparameter
 
 transforms = Compose([ # transformations
@@ -31,7 +35,11 @@ transforms = Compose([ # transformations
 ])
 
 eval_metrics = {
-    
+    "accuracy" : accuracy_score,
+    "f1" : lambda x, y: f1_score(x, y, average="macro"),
+    # "f1" : f1_score,
+    # "recall" : recall_score,
+    # "precision" : precision_score
 }
 
 # HYPERPARAMETERS == END ==
@@ -41,34 +49,48 @@ path = kagglehub.dataset_download("dmitrybabko/speech-emotion-recognition-en")
 dataset_df = pd.DataFrame([
         (wav_path, detect_and_parse(wav_path))
         for wav_path in iter_wavs(Path(path))
-    ], columns=["paths", "emotion"]).dropna()
-dataset_df["emotion"] = dataset_df["emotion"].replace({col:i for i, col in enumerate(dataset_df["emotion"].unique())}) # enocding classes
-NUM_CLASSES = len(dataset_df["emotion"].unique())
+    ], columns=["paths", "emotion"]).dropna().sample(frac=1).reset_index(drop=True)
 
-dataset = AudioDataset(dataset_df, SAMPLING_RATE, CLIP_LENGTH, NUM_CLASSES)
+CLASSES = {col:i for i, col in enumerate(dataset_df["emotion"].unique())}
+dataset_df["emotion"] = dataset_df["emotion"].replace({col:i for i, col in enumerate(dataset_df["emotion"].unique())}) # enocding classes
+NUM_CLASSES = len(CLASSES)
+
+dataset = AudioDataset(dataset_df, SAMPLING_RATE, CLIP_LENGTH, NUM_CLASSES, DEVICE)
 train_dataset, test_dataset, val_dataset = random_split(dataset, SPLITS)
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+#insights about the dataset
+print("original dataset distribution")
+print(dataset_df["emotion"].value_counts()) # shows the frequency of each class
+
+print("train dataset distribution")
+print(dataset_df.iloc[train_dataset.indices]["emotion"].value_counts()) # shows the frequency of each class
+
+print("test dataset distribution")
+print(dataset_df.iloc[train_dataset.indices]["emotion"].value_counts()) # shows the frequency of each class
+
+print(CLASSES) # shows each class with it encoded number
 
 # model parameters == start
-model = TestModel(3, NUM_CLASSES)#...
-optim = AdamW(model.parameters(), lr=1e-3) #...
-loss_fn = CrossEntropyLoss()#...
-# model parameters == end
+# model = TestModel(3, NUM_CLASSES)#...
+# model = model.to(DEVICE)
+# optim = AdamW(model.parameters(), lr=1e-3) #...
+# loss_fn = CrossEntropyLoss()#...
+# # model parameters == end
 
-summary = []
-start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# summary = []
+# start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-for mode, epoch in training_iterator(EPOCHS, TESTING_FREQUENCY):
-    if mode == "training":
-        scores = train(model, train_dataloader, transforms, loss_fn, eval_metrics, optim)
-    if mode == "testing":
-        scores = test(model, test_dataloader, transforms, loss_fn, eval_metrics)
+# for mode, epoch in training_iterator(EPOCHS, TESTING_FREQUENCY):
+#     if mode == "training":
+#         scores = train(model, train_dataloader, transforms, loss_fn, eval_metrics, optim)
+#     if mode == "testing":
+#         scores = test(model, test_dataloader, transforms, loss_fn, eval_metrics)
     
-    summary.append({"mode":mode, "epoch":epoch, **scores})
+#     summary.append({"mode":mode, "epoch":epoch, **scores})
 
-end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-pd.DataFrame(summary).to_csv(start + " -- " + end + ".csv", index=False)
+# pd.DataFrame(summary).to_csv(start + " -- " + end + ".csv", index=False)
